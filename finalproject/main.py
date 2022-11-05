66dadb437ed5239fa9a74249482dbca7b7243592
ZentosBG <113288639+ZentosBG@users.noreply.github.com>
2022-11-05 22:49:52 +0200

    player course

--- finalproject/main.py
+++ finalproject/main.py
@@ -1,5 +1,5 @@
 from pygame import *
-from random import randint
+from random import randint,shuffle
 
 from cards import cards_list
 mixer.init()
@@ -11,19 +11,18 @@ HEIGHT = 600
 window = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Zenterost")
 
+
 place_card_e = {
-    "one_card" : (100,50),
-    "two_card" : (150,50),
-    "three_card" : (200,50)
+    "one_card" : (244,18),
+    "two_card" : (390,17),
+    "three_card" : (534,17)
 }
 
 place_card_p = {
-    "one_card" : (100,150),
-    "two_card" : (150,150),
-    "three_card" : (200,150)
+    "one_card" : (244,236),
+    "two_card" : (390,235),
+    "three_card" : (537,235)
 }
-
-
 
 
 
@@ -42,57 +41,76 @@ class GameSprite(sprite.Sprite):
 
 
 
+
+
+
 class Player:
     def __init__(self):
         self.hp = 500
-        self.ballets = sprite.Group()
         self.cards = []
-        self.cometery_cards = []
 
+    
 
+    
 
-class Enemy():
+class Enemy:
     def __init__(self):
         self.hp = 500
         self.cards = []
-        self.cometery_cards = []
 
 
 
 class Card(GameSprite):
-    def __init__(self, name, card_img, defend, atk):
-        super().__init__('reverse.jpg', x, y, width, height)
+    def __init__(self, name, card_img, defend, atk, x, y):
+        super().__init__('cards/reverse.png', x, y, 120, 180)
         self.name = name
-        self.card_img = card_img
-        self.DEF = 0
-        self.ATK = 0
+        self.card_img = transform.scale(image.load(card_img),(60,100))
+        self.DEF = defend
+        self.ATK = atk
             
-    def move(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
-
+    def move(self, pos):
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
 
 bg_image = transform.scale(image.load("cards/Pole.png"),(WIDTH, HEIGHT))
 
-player = Player()
 clock = time.Clock()
+
+player = Player()
 player.cards = []
-for card in cards_list:
-    new_card = Card(card, cards_list[card]['img'],cards_list[card]['def'],cards_list[card]['atk'])
+
+p_x = 50
+p_y = 400
+name_cards = list(cards_list.keys())
+shuffle(name_cards)
+for card in name_cards:
+    new_card = Card(card, cards_list[card]['img'],cards_list[card]['def'],cards_list[card]['atk'],p_x, p_y)
+    p_x -= 0.5
+    p_y += 0.5 
     player.cards.append(new_card)
 
+
+e_x = 50
+e_y = 50
 enemy = Enemy()
+shuffle(name_cards)
 enemy.cards = []
-for card in cards_list:
-    new_card = Card(card, cards_list[card]['img'],cards_list[card]['def'],cards_list[card]['atk'])
+for card in name_cards:
+    new_card = Card(card, cards_list[card]['img'],cards_list[card]['def'],cards_list[card]['atk'],e_x,e_y)
+    e_x -= 0.5
+    e_y += 0.5 
     enemy.cards.append(new_card)
 
-r_card = 0
-rt = 0
-n_e_c = 18
 
 FPS = 60
+
+r_card = 0
+
+n_e_c = 18
+rt_e = True
+rt_p = True
+
 run = True
 finish = False
 
@@ -103,26 +121,46 @@ while run:
             run = False
     
     if not finish:
+        for card in player.cards:
+            card.draw()
+
+        for card in enemy.cards:
+            card.draw()
 
         if  len(enemy.cards) > 0:
-            if rt == True:
-                enemy.cards[r_card].move(place_card_e[one_card])
+            if rt_e == True:
+                enemy.cards[r_card].move(place_card_e['one_card'])
                 r_card += 1
-                window.blit(enemy.cards[r_card], (place_card_e[two_card]))
+                enemy.cards[r_card].move(place_card_e['two_card'])
                 r_card += 1
-                window.blit(enemy.cards[r_card], (place_card_e[three_card]))
+                enemy.cards[r_card].move(place_card_e['three_card'])
                 r_card += 1
-                rt == False
+                rt_e = False
             else:
                 pass
         else:
             pass
 
+        if  len(player.cards) > 0:
+            if rt_p == True:
+                player.cards[r_card].move(place_card_p['one_card'])
+                r_card += 1
+                player.cards[r_card].move(place_card_p['two_card'])
+                r_card += 1
+                player.cards[r_card].move(place_card_p['three_card'])
+                r_card += 1
+                rt_p = False
+            else:
+                pass
+        else:
+            pass
 
-
-
-    else:
+    else:   
         window.blit(result, (200, 200))
-    enemy.cards.draw(window)
+
+    for card in enemy.cards:
+        card.draw()
+    for card in player.cards:
+        card.draw()
     display.update()
     clock.tick(FPS)
